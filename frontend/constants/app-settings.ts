dc7a91e1c47340837c84c699157f576288ae9330
Timoté Policet <tpolicet@2emotion.com>
2025-01-14 12:54:01 +0100

    clean des settings

---
+++ frontend/constants/app-settings.ts
@@ -0,0 +1,17 @@
+export const AppSettings = [
+  {
+    title: 'Personal Information',
+    path: '/settings/personal',
+    icon: 'solar:user-circle-outline',
+  },
+  {
+    title: 'Administration',
+    path: '/settings/administration',
+    icon: 'heroicons:building-library',
+  },
+  {
+    title: 'Billing',
+    path: '/settings/billing',
+    icon: 'solar:bill-list-outline',
+  },
+];
