cfd8e2aa5854b950590a07055d1f97475e833b54
Timoté Policet <tpolicet@2emotion.com>
2025-01-15 14:27:20 +0100

    renvoi vers auth quand la session expire

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,21 +1,21 @@
 // middleware/auth.ts
-import {useAuthStore} from "~/stores/auth";
+import { useAuthStore } from "~/stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const authStore = useAuthStore()
+    const authStore = useAuthStore();
 
     // Initialiser le store si ce n'est pas déjà fait
     if (!authStore.isAuthenticated && authStore.token) {
-        await authStore.fetchUser()
+        await authStore.fetchUser();
     }
 
-    // Rediriger les utilisateurs non authentifiés vers la page de connexion
-    if (!authStore.isAuthenticated && to.path !== 'auth/login' && to.path !== 'auth/register') {
-        return navigateTo('auth/login')
+    // Rediriger les utilisateurs non authentifiés vers la page de connexion (/login)
+    if (!authStore.isAuthenticated && to.path !== '/login' && to.path !== '/register') {
+        return navigateTo('/auth/login', { replace: true });
     }
 
     // Rediriger les utilisateurs authentifiés loin des pages de connexion et d'inscription
-    if (authStore.isAuthenticated && (to.path === 'auth/login' || to.path === 'auth/register')) {
-        return navigateTo('/')
+    if (authStore.isAuthenticated && (to.path === '/login' || to.path === '/register')) {
+        return navigateTo('/', { replace: true });
     }
-})
+});
